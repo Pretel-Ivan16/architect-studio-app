c9c7d537e2523b62920f6951aa497fa11e01e191
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-14 20:10:45 -0300

    add Stats interface and initial stats data

---
+++ data/stats.ts
@@ -0,0 +1,28 @@
+export interface Stats {
+  id: number
+  number: string
+  label: string
+}
+
+export const stats: Stats[] = [
+  {
+    id: 1,
+    number: "25+",
+    label: "Years of experience"
+  },
+  {
+    id: 2,
+    number: "120",
+    label: "Completed Projects"
+  },
+  {
+    id: 3,
+    number: "15",
+    label: "Design Awards"
+  },
+  {
+    id: 4,
+    number: "8",
+    label: "Countrys"
+  }
+]
