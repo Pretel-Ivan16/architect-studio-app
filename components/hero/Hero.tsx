dc153e85dbc0cec3f3e78fa20a26fd8e43e36d92
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-16 02:57:47 -0300

    refactor Hero component to import heroContent

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -4,16 +4,7 @@ import { HeroDecorations } from './HeroDecorations'
 import { HeroContent } from './HeroContent'
 import { ScrollIndicator } from './ScrollIndicator'
 import { useSmoothScroll } from '@/hooks/useSmoothScroll'
-
-const heroContent = {
-  subtitle: 'Contemporary Architecture',
-  title: 'We design spaces',
-  titleHighlight: 'that inspire',
-  description:
-    'We create architecture that transcendes time, merging functionality with aesthetics that reflect the essence of each project.',
-  buttonLabel: 'Explore Projects',
-  buttonHref: '#projects',
-}
+import { heroContent } from '@/data/hero'
 
 function Hero() {
   const handleNavClick = useSmoothScroll()
