422d73b79f396ad5e0024b2947c92f0df1f5c179
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-16 03:26:12 -0300

    fix: correct import path for Footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import About from "@/components/about/About";
 import Contact from "@/components/contact/Contact";
-import Footer from "@/components/footer";
+import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
 import Philosophy from "@/components/philosophy/Philosophy";
