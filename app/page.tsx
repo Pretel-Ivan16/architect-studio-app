2c6cd367d6052829f31323f72cc1a0ca9a94de5e
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-16 16:23:54 -0300

    refactor: remove unused Footer and Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import About from "@/components/about/About";
 import Contact from "@/components/contact/Contact";
-import Footer from "@/components/footer/Footer";
-import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
 import Philosophy from "@/components/philosophy/Philosophy";
 import Projects from "@/components/projects/Projects";
