2db851b8aa7e98300e8dba5a92b37dcb6d4b5b7d
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-16 02:57:37 -0300

    add heroContent

---
+++ data/hero.ts
@@ -0,0 +1,9 @@
+export const heroContent = {
+  subtitle: 'Contemporary Architecture',
+  title: 'We design spaces',
+  titleHighlight: 'that inspire',
+  description:
+    'We create architecture that transcendes time, merging functionality with aesthetics that reflect the essence of each project.',
+  buttonLabel: 'Explore Projects',
+  buttonHref: '#projects',
+}
