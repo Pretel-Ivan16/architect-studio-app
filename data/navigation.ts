ff78472f05c6a5f2725c72b26bb078621a7d388b
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-16 02:42:28 -0300

    add navigation menu items

---
+++ data/navigation.ts
@@ -0,0 +1,6 @@
+export const menuItems = [
+  { href: '#projects', label: 'Projects' },
+  { href: '#about', label: 'About Me' },
+  { href: '#philosophy', label: 'Philosophy' },
+  { href: '#contact', label: 'Contact' },
+]
