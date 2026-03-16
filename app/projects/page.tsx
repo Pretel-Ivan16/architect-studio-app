d21b899338752832c50310108c0403121820bb32
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-03-16 16:24:04 -0300

    feat: create ProjectsPage component with basic layout

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectsPage() {
+  return (
+    <main>
+      <div className='relative min-h-screen bg-[#ffffff] flex items-center justify-center overflow-hidden'></div>
+    </main>
+  )
+}
+
+export default ProjectsPage 
